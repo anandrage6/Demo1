9fbd48de370ab69ff931c97b730a005252a9109b
Anandrage6 <Anandrage6@gmail.com>
2021-03-29 14:24:50 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/sampleStep/sampleStep.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import {OmniscriptBaseMixin} from 'vlocity_ins/omniscriptBaseMixin';
+
+export default class SampleStep extends OmniscriptBaseMixin(OmniscriptStep) {
+    
+}
